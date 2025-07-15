b95d08cc40bd7dd316f8138e81d773efe9e2ebf8
unknown <rezyek05@gmail.com>
2025-07-15 09:02:20 +0330

    changed a bit

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -3,9 +3,17 @@ import RecipeCard from "./RecipeCard";
 const Recipes = ({ meals }) => {
   return (
     <section className="max-w-6xl mx-auto mt-12 px-4 grid gap-6 md:grid-cols-2 lg:grid-cols-3 grid-cols-1">
-      {meals.map((meal) => (
-        <RecipeCard meals={meals} key={meal["idMeal"]} meal={meal} />
-      ))}
+      {meals ? (
+        meals.map((meal) => (
+          <RecipeCard meals={meals} key={meal["idMeal"]} meal={meal} />
+        ))
+      ) : (
+        <div className="w-full p-5 text-center">
+          <p className="text-xl font-bold">
+            A Recipe Doesn't Exist For That Ingredient
+          </p>
+        </div>
+      )}
     </section>
   );
 };
